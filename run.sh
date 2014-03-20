8b86517a38ef93507412e8b4b58f52cb4828ee76
bob walker <bob.walker@digital.cabinet-office.gov.uk>
2014-03-20 12:00:54 +0000

    use port 8001 so can be run alongside interim production

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd src/
-python -m SimpleHTTPServer &
+python -m SimpleHTTPServer 8001 &
 PID=$!
 
 trap "kill 0" SIGINT SIGTERM EXIT
@@ -8,7 +8,7 @@ sleep 2
 
 cd app/dashboards/
 for dashboard in $(ls *.json | grep -v -e default -e empty ); do
-  open http://localhost:8000/#/dashboard/file/${dashboard}
+  open http://localhost:8001/#/dashboard/file/${dashboard}
 done
 
 wait $PID
